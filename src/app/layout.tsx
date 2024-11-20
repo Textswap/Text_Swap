a13bd6ad3fbf75d3133f55567faf85afbc4d3e8f
dhanielbolosan <dhaniel808123@gmail.com>
2024-11-19 19:37:03 -1000

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from '@/components/Footer';
 import NavBar from '@/components/TextSwapNavbar';
 import Providers from './providers';
+/* import Footer from '@/components/Footer'; */
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
         <Providers>
           <NavBar />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </Providers>
       </body>
     </html>
